0da97b6b3b5b2d4086612a8381b09283117f77a3
Exstaa <exstagithub@gmail.com>
2024-07-03 23:11:33 +0400

    main class uploaded.

---
+++ az/hafizrzazade/bookrecommendation/BookRecommendationApplicationClass.java
@@ -0,0 +1,11 @@
+package az.hafizrzazade.bookrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendationApplicationClass {
+   public static void main(String[] args) {
+	   SpringApplication.run(BookRecommendationApplicationClass.class, args);
+   }
+}
