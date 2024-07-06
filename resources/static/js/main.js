195b7920d8b86094ab6ea602a139bb101ca05f82
Exstaa <exstagithub@gmail.com>
2024-07-06 17:52:58 +0400

    navbar script

--- resources/static/js/main.js
+++ resources/static/js/main.js
@@ -34,3 +34,12 @@ function showBooks(books) {
         }
     }
 }
+
+function toggleMenu() {
+    var x = document.getElementById("navbarLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
