ac5b118011ffd9582704479a3d133f685df8853d
Exstaa <exstagithub@gmail.com>
2024-07-22 22:50:14 +0400

    add comment stage 1

--- resources/static/js/review-page.js
+++ resources/static/js/review-page.js
@@ -28,6 +28,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
+
 async function addReview(e) {
     e.preventDefault();
     let reviewer = document.getElementById("name").value;
@@ -46,7 +47,27 @@ async function addReview(e) {
         },
         body: JSON.stringify(reviewItem)
     })
-    loadReviews();z
+    loadReviews();
+}
+
+async function addComment(e,id){
+    e.preventDefault();
+    let comment = document.getElementById("comment").value;
+    let username = document.getElementById("username").value;
+    let commentItem = {
+           username: username,
+           comment: comment,
+           review: {
+            id: id
+           }
+    }
+    let addCommentResponse = await fetch("http://localhost:8080/api/comments/add", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(commentItem)
+    })
 }
 
 async function loadReviews() {
@@ -56,6 +77,7 @@ async function loadReviews() {
         return response.json()
     })
     let reviewsCon = document.getElementById("reviews-container");
+    let commentsCon = document.getElementById("comment-modals");
     reviewsCon.innerHTML = "";
     for(i = 0; i<reviewsResponse.length;i++){
         let html = `
@@ -63,11 +85,51 @@ async function loadReviews() {
         <span id="reviewer">Title: ${reviewsResponse[i].reviewer}</span>
         <span id="author">Book Name: ${reviewsResponse[i].bookName}</span>
         <span id="rating">Review: ${reviewsResponse[i].review}</span>
+        <button id="openCommentModal${reviewsResponse[i].id}">Add Comment</button>
      </div>
         `
         reviewsCon.innerHTML += html;
 
+     let commentHtml =  `<div id="commentModal${reviewsResponse[i].id}" class="modal">
+        <div class="modal-content">
+          <span class="close" id="close">&times;</span>
+          <form id="modalForm" onsubmit="addComment(event,${reviewsResponse[i].id})">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" placeholder="username:" required><br><br>
+            <label for="comment">Comment:</label>
+            <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Comment:"></textarea><br><br>
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+      </div>`
+
+      commentsCon.innerHTML += commentHtml;
+
+    /*  document.getElementById("openCommentModal"+reviewsResponse[i].id).addEventListener("click", function(){
+        const modal = document.getElementById('commentModal'+ reviewsResponse[i].id);
+        const openModalBtn = document.getElementById('openCommentModal'+ reviewsResponse[i].id);
+        const closeModalBtn = document.getElementById('close');
+    
+        openModalBtn.onclick = function () {
+            modal.style.display = 'block';
+        }
+    
+        closeModalBtn.onclick = function () {
+            modal.style.display = 'none';
+        }
+    
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        }
+      }) */
+
+
     }
+
 }
+
+
 
 loadReviews()
